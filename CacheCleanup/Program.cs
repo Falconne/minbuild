45756fca9e7584a304f3b80ceacd87bae70fc6a6
Anuradha Dissanayake <falconne@gmail.com>
2015-12-13 23:24:26 +1300

    Use NLog

--- CacheCleanup/Program.cs
+++ CacheCleanup/Program.cs
@@ -4,7 +4,7 @@ using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using log4net.Repository.Hierarchy;
+using NLog;
 
 namespace CacheCleanup
 {
@@ -22,13 +22,22 @@ namespace CacheCleanup
 
         private static void CleanCSharpCache(string cacheLocation)
         {
-            var now = DateTime.UtcNow;
-            logger.Info("Current time UTC is " + now);
             foreach (var directory in Directory.GetDirectories(cacheLocation))
             {
                 var di = new DirectoryInfo(directory);
                 if (di.Name == "Precomputed")
                     continue;
+                logger.Info("Check branch version " + directory);
+                CheckAndCleanCsharpVersionDir(directory);
+            }
+        }
+
+        private static void CheckAndCleanCsharpVersionDir(string versionDir)
+        {
+            var now = DateTime.UtcNow;
+            logger.Info("Current time UTC is " + now);
+            foreach (var directory in Directory.GetDirectories(versionDir))
+            {
                 logger.Info("Check " + directory);
                 var completeMarker = Path.Combine(directory, "complete");
                 if (!File.Exists(completeMarker))
@@ -37,13 +46,13 @@ namespace CacheCleanup
                     logger.Info("Completion marker not found, skipping");
                     continue;
                 }
-                var dirTime = di.LastWriteTimeUtc;
+                var dirTime = new DirectoryInfo(directory).LastWriteTimeUtc;
                 logger.Info("Directory write time is " + dirTime);
                 var diff = now - dirTime;
                 logger.Info("Age in days: " + diff.Days);
                 if (diff.Days > 4)
                 {
-                    logger.Info("Direactory is outdated");
+                    logger.Info("Directory is outdated");
                 }
                 else
                 {
@@ -52,8 +61,6 @@ namespace CacheCleanup
             }
         }
 
-
-        static readonly protected log4net.ILog logger =
-           log4net.LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
+        private static readonly Logger logger = LogManager.GetCurrentClassLogger();
     }
 }
