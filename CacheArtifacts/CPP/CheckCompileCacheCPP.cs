8facf52d8869718a8e477d50da664420b5264ef4
Anuradha Dissanayake <falconne@gmail.com>
2015-12-15 23:08:30 +1300

    C++ tlog reading WIP

--- CacheArtifacts/CPP/CheckCompileCacheCPP.cs
+++ CacheArtifacts/CPP/CheckCompileCacheCPP.cs
@@ -6,7 +6,7 @@ using System.Text;
 using System.Threading.Tasks;
 using Microsoft.Build.Framework;
 
-namespace MinBuild.CPP
+namespace MinBuild
 {
     public class CheckCompileCacheCPP : CacheTaskCPP
     {
@@ -27,23 +27,38 @@ namespace MinBuild.CPP
             var tlogCacheLocation = GetCacheDirForHash(cppInput);
             if (string.IsNullOrWhiteSpace(tlogCacheLocation)) return true;
 
-            var clReadTlog = Path.Combine(tlogCacheLocation, "CL.read.1.tlog");
-            if (!File.Exists(clReadTlog))
-            {
-                LogProjectMessage(clReadTlog + " not found, skipping cache");
-                return true;
-            }
-            var compileInputs = ReadTlogFile(clReadTlog, false);
+            var compileInputs = ReadTlogFile(tlogCacheLocation, "CL.read.1.tlog", false);
+            var compileOutputs = ReadTlogFile(tlogCacheLocation, "CL.write.1.tlog", true);
+            var linkInputs = ReadTlogFile(tlogCacheLocation, "link.read.1.tlog", true);
+            var linkOutputs = ReadTlogFile(tlogCacheLocation, "link.read.1.tlog", true);
 
-            var clWriteLog = Path.Combine(tlogCacheLocation, "CL.write.1.tlog");
+            // Real inputs are our source files and linker inputs minus objects from our own
+            // compile (i.e.  other libs).
+            var externalLinkInputs = linkInputs.Where(x => !compileOutputs.Contains(x));
+
+            var realInputs = compileInputs.Concat(externalLinkInputs).ToList();
+            LogProjectMessage("Inputs:");
+            realInputs.ForEach(x => LogProjectMessage("\t" + x));
 
             return true;
         }
 
-        private IEnumerable<string> ReadTlogFile(string file, bool ignoreComments)
+        private IEnumerable<string> ReadTlogFile(string tlogCacheLocation, string name, bool ignoreComments)
         {
-            var lines = File.ReadAllLines(file);
-            return ignoreComments ? lines.Where(x => !x.StartsWith("^")) : lines;
+            var tlog = Path.Combine(tlogCacheLocation, name);
+            if (!File.Exists(tlog))
+            {
+                LogProjectMessage(tlog + " not found, skipping cache");
+                return null;
+            }
+
+            var lines = File.ReadAllLines(tlog);
+            IEnumerable<string> filteredLines;
+            filteredLines = ignoreComments ? 
+                lines.Where(x => !x.StartsWith("^")) : 
+                lines.Select(x => x.Replace("^", ""));
+
+            return filteredLines;
         } 
     }
 }
